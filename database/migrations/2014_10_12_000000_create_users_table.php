7c1d3ccd53123685d1f31a8cba0759411c829771
Rohan Nair <rohan.nair@somaiya.edu>
2020-02-23 23:48:33 +0530

    fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,13 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->integer('role');//teacher, student or HOD
-            $table->integer('department_id');// for now comps, also holds the semester in the lsbs
+            $table->integer('department_id')->default(0);// for now comps, also holds the semester in the lsbs
+            $table->tinyInteger('gender');
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('phone')->unique()->nullable();
+            $table->string('phone', 10)->unique()->nullable();
             $table->string('password');
-            $table->text('img_url');
-            $table->json('description')->index();
+            $table->text('img_url')->nullable();
+            $table->json('description');
             $table->timestamps();
             $table->softDeletes();
             $table->unsignedBigInteger('created_by');
