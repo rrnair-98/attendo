2b74a20fb6d9b6bd8cb2546a353d5556dd2812c8
Rohan Nair <rohan.nair@somaiya.edu>
2020-02-24 16:42:44 +0530

    adding helpers

--- app/Helpers/ResponseHelper.php
+++ app/Helpers/ResponseHelper.php
@@ -1,10 +1,33 @@
 <?php
 
-
 namespace App\Helpers;
 
 
 class ResponseHelper
 {
+    public static function notFound($entity_name, $details = null)
+    {
+        return response()->json([
+            "message" => "No such $entity_name found. $details"
+        ], 404);
+    }
 
+    public static function internalError($details = null)
+    {
+        return response()->json([
+            "message" => "Some internal server error occurred. $details"
+        ], 500);
+    }
+
+
+    public static function forbidden($details = null)
+    {
+        return response()->json([
+            "message" => "Cannot access the given route. $details"
+        ], 401);
+    }
+
+    public static function badRequest($details){
+        return response()->json(['message'=> 'bad request', 'details'=>$details], 400);
+    }
 }
