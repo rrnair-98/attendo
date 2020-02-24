f0c94888ca8308a7d1dad160ea957c3b96ae4ea0
Rohan Nair <rohan.nair@somaiya.edu>
2020-02-24 12:58:18 +0530

    adding base transnactor

--- app/Transactors/BaseTransactor.php
+++ app/Transactors/BaseTransactor.php
@@ -6,5 +6,8 @@ namespace App\Transactors;
 
 class BaseTransactor
 {
-
+    protected const CLASS_NAME = 'BaseTransactor';
+    protected const METHOD_CREATE = 'create';
+    protected const METHOD_DELETE = 'delete';
+    protected const METHOD_UPDATE = 'update';
 }
