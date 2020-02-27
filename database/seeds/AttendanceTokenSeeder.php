f96af60aa6936a31b2421c8dc7b025a4fde388cd
Rohan Nair <rohan.nair@somaiya.edu>
2020-02-27 11:59:30 +0530

    adding attendance table seeder

--- database/seeds/AttendanceTokenSeeder.php
+++ database/seeds/AttendanceTokenSeeder.php
@@ -11,6 +11,21 @@ class AttendanceTokenSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // fills data only for the student... ie cols class_lecture_id and is_present are default/0
+        foreach (\App\TeacherLecture::all() as $teacherLecture){
+            foreach (\App\StudentLecture::where('teacher_lecture_id', $teacherLecture->id)->get() as $subbedStudent){
+                \App\AttendanceToken::create([
+                    'token' => hash('sha256', \Carbon\Carbon::now()->timestamp.$subbedStudent->user_id.$subbedStudent->id),
+                    'expires_at'    => \Carbon\Carbon::now()->addMinutes(\App\AttendanceToken::MAX_EXPIRY_IN_MINUTES),
+                    'created_by'    => $subbedStudent->user_id,
+                ]);
+            }
+        }
+        $i=0;
+        while(++$i<10) {
+            \App\AttendanceToken::inRandomOrder()->skip(20)->take(10)->delete();
+        }
+
+
     }
 }
