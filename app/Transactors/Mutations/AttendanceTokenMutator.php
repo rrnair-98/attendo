689d37ea769a42cd7c1ae6009e26c33b272e793c
Rohan Nair <rohan.nair@somaiya.edu>
2020-02-27 13:39:55 +0530

    adding generateTOken

--- app/Transactors/Mutations/AttendanceTokenMutator.php
+++ app/Transactors/Mutations/AttendanceTokenMutator.php
@@ -1,9 +1,19 @@
 <?php
 namespace App\Transactors\Mutations;
 
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+
 class AttendanceTokenMutator extends BaseMutator{
     public function __construct()
     {
         parent::__construct('App\AttendanceToken', 'id');
     }
+
+    public function generateToken(User $user){
+        return hash('sha256', $user->email.$user->created_at.Str::random().Carbon::now()->timestamp);
+    }
+
+
 }
