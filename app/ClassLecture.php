73a361c838ac7f04adc71446cf110e66b684fdbb
Rohan Nair <rohan.nair@somaiya.edu>
2020-02-27 13:40:23 +0530

    adding valdiation rules

--- app/ClassLecture.php
+++ app/ClassLecture.php
@@ -3,9 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ClassLecture extends Model
 {
     //
+    use SoftDeletes;
     public $timestamps = true;
+    protected $fillable = ['teacher_lecture_id', 'created_by', 'updated_by', 'deleted_at', 'description'];
+    public const CREATE_VALIDATION_RULES = array(
+        'teacher_lecture_id' => 'exists:teacher_lectures,id',
+        'created_by'    => 'exists:users,id',
+    );
+    public const UPDATE_VALIDATION_RULES = array(
+        'deleted_at' => 'date_format:Y-m-d H:i:s',
+        'updated_by' => 'exist:users,id'
+    );
 }
