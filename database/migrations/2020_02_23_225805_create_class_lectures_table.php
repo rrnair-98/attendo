4e3d5c5012475ca356c2c665835a4298cc4c9cf0
Rohan Nair <rohan.nair@somaiya.edu>
2020-02-27 13:38:47 +0530

    indexing teacher lecture od

--- database/migrations/2020_02_23_225805_create_class_lectures_table.php
+++ database/migrations/2020_02_23_225805_create_class_lectures_table.php
@@ -17,11 +17,12 @@ class CreateClassLecturesTable extends Migration
         // this way you can get total lecture count also
         Schema::create('class_lectures', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('teacher_lecture_id');// a teacher lecture id
+            $table->unsignedBigInteger('teacher_lecture_id')->index();// a teacher lecture id
             $table->unsignedBigInteger('created_by');
             $table->unsignedBigInteger('updated_by')->nullable();
             $table->longText('description')->nullable(); // a JSON col that could be used later to store what topics the teacher covered
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
